8e350f3789936a7b0a366c49e3fdbced3d5408b6
Orumgbe <orumgbeugbekile@gmail.com>
2023-03-12 14:08:07 -0700

    fix

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -10,7 +11,6 @@ class BaseModel:
     def __init__(self, *args, **kwargs):
         """Initialization of a new model object"""
         if not kwargs:
-            from models import storage
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
@@ -30,7 +30,6 @@ class BaseModel:
 
     def save(self):
         """Updates public instance attribute with current datetime"""
-        from models import storage
         self.updated_at = datetime.now()
         storage.save()
 
